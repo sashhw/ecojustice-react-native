509f4059ba4743d61af027c2b963c4adad8891f9
swalkowski <swalkowski@foxtrotco.com>
2024-01-10 10:31:03 -0600

    added code to support viewing "cases" already stored in firebase

--- views/CasesScreen.js
+++ views/CasesScreen.js
@@ -1,11 +1,28 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { View, Text, Button, ScrollView } from "react-native";
+import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
 import app from "../FirebaseApp";
 
 const firestore = getFirestore(app);
 
 const CasesScreen = () => {
+  const [cases, setCases] = useState([]);
+  useEffect(() => {
+    const fetchCases = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(firestore, "cases"));
+        const casesData = [];
+        querySnapshot.forEach((doc) => {
+          casesData.push({ id: doc.id, ...doc.data() });
+        });
+        setCases(casesData);
+      } catch (error) {
+        console.Console.error("Error fetching documents: ", error);
+      }
+    };
+    fetchCases();
+  }, []);
+
   const addCase = async () => {
     try {
       const docRef = await addDoc(collection(firestore, "cases"), {
@@ -21,10 +38,17 @@ const CasesScreen = () => {
   };
 
   return (
-    <View>
-      <Text>Cases</Text>
-      <Button title="Add Case" onPress={() => addCase()} />
-    </View>
+    <ScrollView>
+      <View>
+        <Text> Existing Cases: </Text>
+        {cases.map((c) => (
+          <Text
+            key={c.id}
+          >{`Name: ${c.name}, Year: ${c.year}, Location: ${c.location}`}</Text>
+        ))}
+        <Button title="Add Case" onPress={() => addCase()} />
+      </View>
+    </ScrollView>
   );
 };
 
